c14f1b3344c5b0e64c3533152054314b5f20a7ff
fabiomdiniz <fabiomachadodiniz@gmail.com>
2010-04-07 19:52:20 +0000

    Compactação do logo uso de mais dados no site

--- sunnytrends/main.py
+++ sunnytrends/main.py
@@ -25,40 +25,45 @@ import stats
 
 class MainPage(webapp.RequestHandler):
     def get(self):
+        
+        len_bd = max_sp.Max_sp.all().count()
+              
         bovespa_query = bovespa.Bovespa.all().order('-date')
-        _bovespa = bovespa_query.fetch(10)
+        _bovespa = bovespa_query.fetch(len_bd - 1)
 
         _bovespa = [item.value for item in _bovespa]
         
         dollar_query = dollar.Dollar.all().order('-date')
-        _dollar = dollar_query.fetch(10)
+        _dollar = dollar_query.fetch(len_bd - 1)
 
         _dollar = [item.value for item in _dollar]
         
         dowjones_query = dowjones.Dowjones.all().order('-date')
-        _dowjones = dowjones_query.fetch(10)
+        _dowjones = dowjones_query.fetch(len_bd - 1)
 
         _dowjones = [item.value for item in _dowjones]
         
         max_sp_query = max_sp.Max_sp.all().order('-date')
-        _max_sp = max_sp_query.fetch(11)
+        _max_sp = max_sp_query.fetch(len_bd)
 
         _max_sp = [item.value for item in _max_sp]
         
         nasdaq_query = nasdaq.Nasdaq.all().order('-date')
-        _nasdaq = nasdaq_query.fetch(10)
+        _nasdaq = nasdaq_query.fetch(len_bd - 1)
 
         _nasdaq = [item.value for item in _nasdaq]
 
         petr4_query = petr4.Petr4.all().order('-date')
-        _petr4 = petr4_query.fetch(10)
+        _petr4 = petr4_query.fetch(len_bd - 1)
 
         _date = [datetime.date(int(p.date[:4]), int(p.date[4:6]), int(p.date[6:])).strftime('%d-%b') for p in _petr4]
 
         _petr4 = [item.value for item in _petr4]
 
-        pearson_report  = stats.make_report(pearsonr, [_nasdaq, _bovespa, _dollar, _dowjones, _max_sp[:-1], _petr4])
-        spearman_report = stats.make_report(spearmanr,[_nasdaq, _bovespa, _dollar, _dowjones, _max_sp[:-1], _petr4])        
+        pearson_report  = stats.make_sunny_report(pearsonr, [_nasdaq, _bovespa, _dollar, _dowjones, _max_sp[:-1], _petr4], \
+                                                  4, ['Nasdaq', 'Bovespa', 'USD/BRL exchange', 'Dow Jones', 'São Paulo\'s high forecast', 'PETR4 stocks'])
+        spearman_report = stats.make_sunny_report(spearmanr,[_nasdaq, _bovespa, _dollar, _dowjones, _max_sp[:-1], _petr4], \
+                                                  4, ['Nasdaq', 'Bovespa', 'USD/BRL exchange', 'Dow Jones', 'São Paulo\'s high forecast', 'PETR4 stocks'])
 
         p_idx = pearson_report['values'].index(max(pearson_report['values']))
         s_idx = spearman_report['values'].index(max(spearman_report['values']))
--- sunnytrends/stats.py
+++ sunnytrends/stats.py
@@ -2,6 +2,9 @@
 
 from deps.statlib.stats import *
 
+def list_difference(list1, list2):
+    return list(set(list1)-set(list2))
+
 def make_report(func, data_matrix):
     report = {'values':[], 'first':[], 'second':[]}    
     next_data = 1    
@@ -12,4 +15,13 @@ def make_report(func, data_matrix):
             report['first'].append(data_list_1[0].__class__.__name__)
             report['second'].append(data_matrix[next_data + i][0].__class__.__name__)
         next_data += 1
+    return report
+    
+def make_sunny_report(func, data_matrix, sunny_index, keys):
+    report = {'values':[], 'data':[]}
+    for data_list in data_matrix:
+        if data_matrix.index(data_list) != sunny_index:
+            report['values'].append(func([data for data in data_list], \
+                                         [data for data in data_matrix[sunny_index]])[0])
+            report['data'].append(keys[data_matrix.index(data_list)])
     return report
