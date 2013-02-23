45dc50d8fe14ed0ef27bcc466a1790e7c1737199
Fábio Machado DIniz <fabiomachadodiniz@gmail.com>
2013-02-23 16:29:20 -0300

    Trying to fix everything

--- sunnytrends/dowjones.py
+++ sunnytrends/dowjones.py
@@ -3,12 +3,16 @@
 from google.appengine.ext import db
 from time import strftime
 import urllib
-from deps import ystockquote
+from deps.BeautifulSoup import BeautifulSoup
+
 
 class Dowjones(db.Model):
-  value = db.FloatProperty()
-  date = db.StringProperty()
+    value = db.FloatProperty()
+    date = db.StringProperty()
+
 
 def get():
-    return [float(ystockquote.get_price('INDU')), strftime('%Y%m%d')]
-
+    url = 'http://finance.yahoo.com/q?s=indu'
+    soup = BeautifulSoup(urllib.urlopen(url))
+    value = float(soup('span', attrs={'id': 'yfs_l10_^dji'})[0].text.replace(',', ''))
+    return [value, strftime('%Y%m%d')]
--- sunnytrends/max_sp.py
+++ sunnytrends/max_sp.py
@@ -3,16 +3,26 @@
 from google.appengine.ext import db
 from datetime import datetime, timedelta
 import urllib
-import os
-import xml.dom.minidom
+import json
+#import xml.dom.minidom
+
 
 class Max_sp(db.Model):
-  value = db.FloatProperty()
-  date = db.StringProperty()
+    value = db.FloatProperty()
+    date = db.StringProperty()
+
+#def get():
+#    html = urllib.urlopen('http://xoap.weather.com/weather/local/BRXX0232?dayf=2&link=xoap&prod=xoap&par=1105827508&key=783abed588a1bdac&unit=m')
+#    dom = xml.dom.minidom.parseString(html.read())
+#    max_sp = float(dom.getElementsByTagName('hi')[1].firstChild.data)
+#    tomorrow = datetime.today() + timedelta(1)
+#    return [max_sp, str(tomorrow)[:10].replace('-','')]
+
 
 def get():
-    html = urllib.urlopen('http://xoap.weather.com/weather/local/BRXX0232?dayf=2&link=xoap&prod=xoap&par=1105827508&key=783abed588a1bdac&unit=m')
-    dom = xml.dom.minidom.parseString(html.read())
-    max_sp = float(dom.getElementsByTagName('hi')[1].firstChild.data)
+    code = datetime.today().strftime('%Y%m%d')
+    url = 'http://api.wunderground.com/api/8cf7aa1a59431128/history_' + code + '/q/BR/Sao_Paulo.json'
+    info = json.loads(urllib.urlopen(url).read())
+    max_sp = float(info['history']['dailysummary'][0]['maxtempm'])
     tomorrow = datetime.today() + timedelta(1)
-    return [max_sp, str(tomorrow)[:10].replace('-','')]
+    return [max_sp, str(tomorrow)[:10].replace('-', '')]
